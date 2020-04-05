60f509d652406a33a8bcd1de3f87ea9f548610fe
Vincent <vincent.dagoury@gmail.com>
2020-04-05 16:57:18 +0200

    Add arguments processing

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,27 @@
+import arg from 'arg'
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--git': Boolean,
+            '--yes': Boolean,
+            '--install': Boolean,
+            '-g': '--git',
+            '-y': '--yes',
+            '-i': '--install',
+        }, {
+            argv: rawArgs.slice(2),
+        }
+    )
+    return {
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false,
+    }
+
+}
 export function cli(args)  {
-    console.log('This is my CLI')
-    console.log(args)
+    let options = parseArgumentsIntoOptions(args)
+    console.log(options)
 }
