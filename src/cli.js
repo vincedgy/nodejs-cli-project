322f96f6caa2842a6f2216d95d842769b7436088
Vincent <vincent.dagoury@gmail.com>
2020-04-05 17:05:57 +0200

    Adding inquirer

--- src/cli.js
+++ src/cli.js
@@ -1,27 +1,68 @@
-import arg from 'arg'
+import arg from "arg";
+import inquirer from "inquirer";
 
 function parseArgumentsIntoOptions(rawArgs) {
-    const args = arg(
-        {
-            '--git': Boolean,
-            '--yes': Boolean,
-            '--install': Boolean,
-            '-g': '--git',
-            '-y': '--yes',
-            '-i': '--install',
-        }, {
-            argv: rawArgs.slice(2),
-        }
-    )
-    return {
-        skipPrompts: args['--yes'] || false,
-        git: args['--git'] || false,
-        template: args._[0],
-        runInstall: args['--install'] || false,
+  const args = arg(
+    {
+      "--git": Boolean,
+      "--yes": Boolean,
+      "--install": Boolean,
+      "-g": "--git",
+      "-y": "--yes",
+      "-i": "--install"
+    },
+    {
+      argv: rawArgs.slice(2)
     }
-
+  );
+  return {
+    skipPrompts: args["--yes"] || false,
+    git: args["--git"] || false,
+    template: args._[0],
+    runInstall: args["--install"] || false
+  };
 }
-export function cli(args)  {
-    let options = parseArgumentsIntoOptions(args)
-    console.log(options)
+
+async function promptForMissingOptions(options) {
+  const defaultTemplate = "JavaScript";
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: opeion.template || defaultTemplate
+    };
+  }
+
+  const questions = [];
+
+  if (!options.template) {
+    questions.push({
+      type: "list",
+      name: "template",
+      message: "Please choose which project template to use",
+      choices: ["JavaScript", "TypeScript"],
+      default: defaultTemplate
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: "config",
+      name: "git",
+      message: "Initialize git repository",
+      default: false
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git
+  };
+}
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  console.log(options);
 }
